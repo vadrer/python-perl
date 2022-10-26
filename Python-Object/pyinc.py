ef073179eca171b21d21da9d4f9bd36167267174
vadim <wadrer@mail.ru>
2022-10-26 22:10:32 +0300

    some linux compatibility

--- Python-Object/pyinc.py
+++ Python-Object/pyinc.py
@@ -6,4 +6,4 @@ import sys
 if sys.platform == 'win32':
 	print(os.path.join(sys.prefix, 'include'))
 else:
-	print(os.path.join(sys.prefix, 'include', 'python' + sys.version[:3]))
+	print(os.path.join(sys.prefix, 'include', 'python' + str(sys.version_info.minor)))
